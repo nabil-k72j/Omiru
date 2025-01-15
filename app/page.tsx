b15e96e5d5d0337f7b3525b98bc22fd428f943bb
nabil-k72j <nabilmalki400@gmail.com>
2025-01-15 17:29:04 +0100

    add dark/light mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/theme-provider/ModeToggle";
 
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center p-24">
+    <div className="h-screen flex flex-col items-center p-24">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Omiru</h1>
+      <ModeToggle />
     </div>
   );
 }
