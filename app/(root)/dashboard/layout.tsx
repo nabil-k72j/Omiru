495cc0886ff0b2403df5449b2d9494f0a10d75fd
nabil-k72j <nabilmalki400@gmail.com>
2025-01-15 21:30:23 +0100

    dashboard page & layout

---
+++ app/(root)/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Omiru | Dashboard",
+  description:
+    "Omiru is a versatile web application designed to simplify everyday digital tasks. With features like quick image uploads, an online HTML editor, and a note management system, Omiru serves as your all-in-one productivity platform.",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="min-h-screen flex justify-center p-24">{children}</main>
+  );
+}
