7a2a8779465e1b53c01fe349e20004c8200b7e78
nabil-k72j <nabilmalki400@gmail.com>
2025-01-17 22:40:14 +0100

    remove last sign in from userschema

--- models/user.model.ts
+++ models/user.model.ts
@@ -2,12 +2,12 @@ import z from "zod"
 
 
 export const UserSchema = z.object({
+    clerkId: z.string(),
     userName: z.string().optional().nullable(),
     firstName: z.string().optional().nullable(),
     lastName: z.string().optional().nullable(),
     avatar: z.string().optional(),
     email: z.string().email(),
-    lastSignIn: z.number().optional().nullable(),
 })
 
 export type TUser = z.infer<typeof UserSchema>
