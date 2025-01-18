73b641e4c5728d3370259e585b8d6d5e0bbb11f3
nabil-k72j <nabilmalki400@gmail.com>
2025-01-18 23:03:16 +0100

    consts

---
+++ lib/consts.ts
@@ -0,0 +1,18 @@
+
+export const links = [
+    {
+        href: "/images",
+        icon: "material-symbols:perm-media",
+        text: "Media Gallery"
+    },
+    {
+        href: "/editor",
+        icon: "oui:editor-code-block",
+        text: "Html editor"
+    },
+    {
+        href: "/notes",
+        icon: "solar:notes-bold",
+        text: "note taker"
+    },
+]
