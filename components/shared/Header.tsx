3c01dc5dd7c9fae1902ced625709825ab46ac752
nabil-k72j <nabilmalki400@gmail.com>
2025-01-23 23:15:52 +0100

    turned the logo into a clickable link

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,9 +7,9 @@ export default function Header() {
   return (
     <header className="fixed w-full top-0 left-0 z-50">
       <section className="p-4 sm:p-8 flex justify-center sm:justify-between items-center">
-        <h1 className="scroll-m-20 text-5xl font-semibold tracking-tight">
+        <Link href={"/"} className="scroll-m-20 text-5xl font-semibold tracking-tight">
           <span className="text-primary">O</span>miru
-        </h1>
+        </Link>
 
         <div className="hidden sm:flex items-center gap-4">
           <SignedIn>
